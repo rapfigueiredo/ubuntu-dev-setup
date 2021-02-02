896f3a7b6a3ea17aebee76ded475e21e2ca3cf42
Rogério Antonio Peixto Figueiredo <rogerio.a.p.figueiredo@gmail.com>
2021-02-02 00:29:39 -0300

    config asdf

---
+++ asdf/asdf.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
+echo ". $HOME/.asdf/asdf.sh" >> ~/.bashrc
+echo ". $HOME/.asdf/completions/asdf.bash" >> ~/.bashrc
+
+
---
+++ asdf/plugin.sh
@@ -0,0 +1,9 @@
+asdf plugin-add java https://github.com/halcyon/asdf-java.git
+asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
+bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-previous-release-team-keyring'
+asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
+asdf plugin-add maven
+
+asdf plugin list
