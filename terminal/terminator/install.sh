91005a1f3bd570f4e760ab50c8d8b7dbb0df9a70
Rogério Figueiredo <rogerio.a.p.figueiredo@gmail.com>
2023-12-20 17:17:41 -0300

    remove terminal

--- terminal/terminator/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sudo add-apt-repository ppa:gnome-terminator
-sudo apt-get update
-sudo apt-get install terminator
--- terminal/terminator/uninstall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sudo apt remove terminator
-sudo apt remove --auto-remove terminator
