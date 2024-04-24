75048ee07e7c7d6e062529ede041c8440f77f4e3
Rog[erio Figueiredo <rogerio.a.p.figueiredo@gmail.com>
2024-04-24 16:08:48 -0300

    fix minikube app name

--- minikube/install.sh
+++ minikube/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #https://minikube.sigs.k8s.io/docs/start/
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikubels
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
 rm -rf minikube-linux-amd64
