38cca11a76018c11d755164e6ce4dea995b04126
Rog[erio Figueiredo <rogerio.a.p.figueiredo@gmail.com>
2024-04-24 16:32:58 -0300

    fix bash source  and alias

--- minikube/install.sh
+++ minikube/install.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 #https://minikube.sigs.k8s.io/docs/start/
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 rm -rf minikube-linux-amd64
 
 cat ~/.bashrc | grep  "minikube start" | wc -l 
-[ "$(cat ~/.bashrc | grep  "minikube start" | wc -l)" -eq "0" ] && { alias kubectl="minikube kubectl --"; alias k="kubectl" }
+[ "$(cat ~/.bashrc | grep  "minikube start" | wc -l)" -eq "0" ] && { alias kubectl="minikube kubectl --"; alias k="kubectl"; }
 [ "$(cat ~/.bashrc | grep  "minikube start" | wc -l)" -eq "0" ] &&  echo "minikube start" >> ~/.bashrc
 
 source ~/.bashrc
