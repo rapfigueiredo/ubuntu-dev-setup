5e0e72ce05d39805c4c6ce88144860d643c19918
Rog[erio Figueiredo <rogerio.a.p.figueiredo@gmail.com>
2024-04-24 16:43:12 -0300

    add test alias

--- minikube/install.sh
+++ minikube/install.sh
@@ -7,4 +7,12 @@ rm -rf minikube-linux-amd64
 [ "$(cat ~/.bashrc | grep  "kubectl"  | wc -l)" -eq "0" ] && { alias kubectl="minikube kubectl --"; alias k="kubectl"; }
 [ "$(cat ~/.bashrc | grep  "minikube start" | wc -l)" -eq "0" ] &&  echo "minikube start" >> ~/.bashrc
 
+cat ~/.bashrc
+
 source ~/.bashrc
+
+minikube kubectl -- -version
+
+kubectl cluster-info
+
+k get nodes
