b1cb06b7c87eee11544466be40b2d2eec48afaff
Rog[erio Figueiredo <rogerio.a.p.figueiredo@gmail.com>
2024-04-24 16:40:01 -0300

    fix alias kubectl

--- minikube/install.sh
+++ minikube/install.sh
@@ -4,8 +4,7 @@ curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 rm -rf minikube-linux-amd64
 
-cat ~/.bashrc | grep  "minikube start" | wc -l 
-[ "$(cat ~/.bashrc | grep  "minikube start" | wc -l)" -eq "0" ] && { alias kubectl="minikube kubectl --"; alias k="kubectl"; }
+[ "$(cat ~/.bashrc | grep  "kubectl"  | wc -l)" -eq "0" ] && { alias kubectl="minikube kubectl --"; alias k="kubectl"; }
 [ "$(cat ~/.bashrc | grep  "minikube start" | wc -l)" -eq "0" ] &&  echo "minikube start" >> ~/.bashrc
 
 source ~/.bashrc
